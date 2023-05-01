bb6c65b22a108af972b652319af8c2a5a50a7a52
Nadezhda Krivorotova <chaus-fox@list.ru>
2023-05-01 16:48:38 +0600

    Gitignore added

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 16859; // стоимость билета
+
+        int mile = 20; // стоимость мили
+
+
+        System.out.println("Начисленно миль:");
+        System.out.println(amount / mile);
+} }
