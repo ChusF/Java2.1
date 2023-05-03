ea148a1f0ac7a8f4b51f6a160e1b3812ae029860
ChusF <130755386+ChusF@users.noreply.github.com>
2023-05-03 20:30:46 +0600

    Update and rename src/Main.java to Src/Main.java

--- Src/Main.java
+++ Src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         int Amount = 16859; // стоимость билета
-
         int Mile = 20; // стоимость мили
 
         System.out.println("Начисленно миль:");
